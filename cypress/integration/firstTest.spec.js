fd745840ae575f8cb4abdcf061043ed4a6181d79
Sacha Wharton <sacha@gmail.com>
2020-07-30 18:11:28 +0200

    api testing

--- cypress/integration/firstTest.spec.js
+++ cypress/integration/firstTest.spec.js
@@ -3,6 +3,9 @@
 describe('Test with backend', () => {
 
   beforeEach('login to the app', () => {
+    cy.server()
+    //adding fixtures
+    cy.route('GET', '**/tags','fixture:tags.json')
     cy.loginToApplication()
   })
 
@@ -19,7 +22,7 @@ describe('Test with backend', () => {
     cy.contains('New Article').click()
     cy.get('[formcontrolname="title"]').type('This is a title')
     cy.get('[formcontrolname="description"]').type('This is a description')
-    cy.get('[formcontrolname="body"]').type('This is the body of the article')
+    cy.get('[formcontrolname="body"]').type('This is the body of the Article')
     cy.contains('Publish Article').click()
 
     //waiting on the response from the object
@@ -27,8 +30,24 @@ describe('Test with backend', () => {
     cy.wait('@postArticles')
     cy.get('@postArticles').then(xhr => {
       console.log(xhr)
+      expect(xhr.status).to.equal(200)
+      expect(xhr.request.body.article.body).to.equal('This is the body of the Article')
+      expect(xhr.response.body.article.description).to.equal('This is a description')
     })
 
+  })
+
+  it('should give the tags with the routing object', () => {
+    cy.get('.tag-list')
+    .should('contain','cypress')
+    .and('contain','automation')
+    .and('contain','testing')
+  })
+
+  it('verify global feed likes count', () => {
+
+    cy.route('GET', '**/articles*','{"articles":[],"articlesCount":0}')
+    cy.route('GET', '**/articles*','fixture:articles.json')
   })
 
 })
